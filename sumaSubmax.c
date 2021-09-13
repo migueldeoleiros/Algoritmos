abbd37dae1851ab87a3d10e42db03516e051d0f7
migueldeoleiros <migueldeoleiros@gmail.com>
2021-09-13 12:11:03 +0200

    ejercicio con aleatorios sin formato

--- sumaSubmax.c
+++ sumaSubmax.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define N 5
+
+void inicializar_semilla(){
+    srand(time(NULL));
+}
+void aleatorio(int v[], int n){
+    int i, m=2*n+1;
+    for (i=0;i<n;i++){
+        v[i] = (rand() % m) - n;
+    }
+}
  
 int sumaSubMax1(int v[],int n)
 {
@@ -38,8 +50,22 @@ int main() {
     out2 = sumaSubMax2(v, N);
 
     printf("sumaSubMax1 = %d \n", out1);
-    printf("sumaSubMax2 = %d \n", out2);
+    printf("sumaSubMax2 = %d \n \n", out2);
  
+// aleatorio
+
+    inicializar_semilla();
+    aleatorio(v, N);
+
+    out1 = sumaSubMax1(v, N);
+    out2 = sumaSubMax2(v, N);
+
+    for (int i=0; i<N;i++){
+        printf("%d ", v[i]);
+    }
+
+    printf("sumaSubMax1 = %d \n", out1);
+    printf("sumaSubMax2 = %d \n", out2);
  
     return 0;
 }

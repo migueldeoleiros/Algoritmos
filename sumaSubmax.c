b4213a9a1132c65ba3175d4a1864c1090efeb38c
SiegEngel <90610062+SiegEngel@users.noreply.github.com>
2021-09-13 11:34:34 +0200

    Update sumaSubmax.c

--- sumaSubmax.c
+++ sumaSubmax.c
@@ -20,3 +20,26 @@ int main() {
  
     return 0;
 }
+
+
+//////////////////////////////////////////////////
+
+
+    void sumaSubMax1(int pepe[],int n)
+    {
+        int sumamax=0;
+        for (int i = 1; i < n; ++i) {
+            int estasuma=0;
+            for (int j = 1; j < n; ++j) {
+                estasuma=estasuma+n;
+                if (estasuma>sumamax)
+                    sumamax=estasuma;
+            }
+        }
+    }
+    int main (void)
+    {
+     
+     
+       return 0;
+    }

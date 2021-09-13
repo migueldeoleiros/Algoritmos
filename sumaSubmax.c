d67a7baa3d7f52383253018c30f4fa6aaf2bdb03
migueldeoleiros <migueldeoleiros@gmail.com>
2021-09-13 11:27:30 +0200

    added function sumaSubmax2

--- sumaSubmax.c
+++ sumaSubmax.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
  
+int sumaSubMax2 (int v[], int n){
+    int estaSuma, sumaMax =0;
+
+    for (int j = 0; j<n; j++){
+        estaSuma = estaSuma + v[j];
+        if (estaSuma > sumaMax){
+            sumaMax = estaSuma;
+        }else if (estaSuma < 0){
+            estaSuma = 0;
+        }
+    }
+    return sumaMax;
+}
+
 int main() {
  
  

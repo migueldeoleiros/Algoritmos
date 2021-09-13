f7dd868c4dbda00c9a064dfa34e2b680e3871f09
migueldeoleiros <migueldeoleiros@gmail.com>
2021-09-13 11:53:46 +0200

    ejercicio 1 y 2 funcionan

--- sumaSubmax.c
+++ sumaSubmax.c
@@ -4,10 +4,11 @@
  
 int sumaSubMax1(int v[],int n)
 {
-    int sumamax=0;
+    int sumamax=0, estasuma=0;
+
     for (int i = 0; i < n; ++i) {
-        int estasuma=0;
-        for (int j = 0; j < n; ++j) {
+        estasuma=0;
+        for (int j = i; j < n; ++j) {
             estasuma=estasuma+v[j];
             if (estasuma>sumamax)
                 sumamax=estasuma;
@@ -17,7 +18,7 @@ int sumaSubMax1(int v[],int n)
 }
 
 int sumaSubMax2 (int v[], int n){
-    int estaSuma, sumaMax =0;
+    int estaSuma =0 , sumaMax =0;
 
     for (int j = 0; j<n; j++){
         estaSuma = estaSuma + v[j];
@@ -32,7 +33,7 @@ int sumaSubMax2 (int v[], int n){
 
 int main() {
     int out1,out2;
-    int v[N] = {4,0,9,2,5};
+    int v[N] = {7,-5,6,7,-7};
     out1 = sumaSubMax1(v, N);
     out2 = sumaSubMax2(v, N);
 

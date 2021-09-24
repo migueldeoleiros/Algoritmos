4d9064755218fc4a3cecf9fac2cbc3ec8637cd39
migueldeoleiros <migueldeoleiros@gmail.com>
2021-09-24 09:20:26 +0200

    arreglado el código para que se vea bien

--- p1/sumaSubmax.c
+++ p1/sumaSubmax.c
@@ -14,6 +14,14 @@ void aleatorio(int v[], int n){
     }
 }
 
+double microsegundos() {  /* obtiene la hora del sistema en microsegundos */
+    struct timeval t;
+    if (gettimeofday(&t, NULL) < 0 ){
+        return 0.0;
+    }
+    return (t.tv_usec + t.tv_sec * 1000000.0);
+}
+
 int sumaSubMax1(int v[],int n)
 {
     int sumaMax=0, estaSuma=0;
@@ -44,21 +52,41 @@ int sumaSubMax2 (int v[], int n){
 }
 
 void printResults (int v[], int n){
-    printf("[ ");
+    printf("[");
     for (int i=0; i<n;i++){
-        printf("%d ", v[i]);
+        printf("%3d", v[i]);
     }
     printf("]");
-    printf("    %d    %d \n", sumaSubMax1(v, n), sumaSubMax2(v, n));
+    printf("%6d%6d \n", sumaSubMax1(v, n), sumaSubMax2(v, n));
 
 }
 
-double microsegundos() {  /* obtiene la hora del sistema en microsegundos */
-    struct timeval t;
-    if (gettimeofday(&t, NULL) < 0 ){
-        return 0.0;
+void printTest1(){
+    int n=5;
+    int v[][5] = {
+		{4,0,9,2,5},
+		{-2,-1,-9,-7,-1},
+		{9,-2,1,-7,-8},
+		{7,-5,6,7,-7},
+		{15,-2,-5,-4,16},
+		{-9,2,-5,-4,6}
+	};
+ 
+    printf("test con vectores dados \n");
+    for(int i=0; i<6;i++){
+        printResults(v[i], n);
     }
-    return (t.tv_usec + t.tv_sec * 1000000.0);
+
+}
+
+void printAleatorio(){
+    int n = 5;
+    int vector[n];
+    inicializar_semilla();
+    aleatorio(vector, n);
+
+    printf("Test con números aleatorios \n");
+    printResults(vector, n);
 }
 
 double testSumaSubmax1(int vector[], int numero){
@@ -91,7 +119,7 @@ double testSumaSubmax1(int vector[], int numero){
     t2=tb-ta;
     t=(t1-t2)/k;
     printf("(*)");
-  }
+  }else printf("   ");
   return t;
 }
 
@@ -125,57 +153,60 @@ double testSumaSubmax2(int vector[], int numero){
     t2=tb-ta;
     t=(t1-t2)/k;
     printf("(*)");
-  }
+  }else printf("   ");
   return t;
 }
 
-int main() {
+void printsumasubmax1(){
     double t =0; 
-    int n=5;
     double tsub, taj, tsob;
 
-    int v[] = {7,-5,6,7,-7};
-    printResults(v, n);
-
-// aleatorio
-
-    inicializar_semilla();
-    aleatorio(v, n);
-
-    printResults(v, n);
-
     printf("Test con SumaSubMax1 \n");
-	printf("%4s%19s%20s%21s%19s \n", "n", "t(n)", "t(n)/n^1.8", "t(n)/n^2", "t(n)/n^2.2");
-    n = 500;
-    while(n <= 32000){
+	printf("%7s%17s%20s%20s%19s \n", "n", "t(n)", "t(n)/n^1.8", "t(n)/n^2", "t(n)/n^2.2");
+
+    for(int n = 500; n <= 32000; n*=2){
         int vector[n];
         t = testSumaSubmax1(vector, n);
 
         tsub=t/pow(n,1.8);
 		taj=t/pow(n,2);
 		tsob=t/pow(n,2.2);
-		printf("%5d%17.2f%18.6f%22.6f%18.6f\n", n, t, tsub, taj, tsob);
-
-        n*=2;
+		printf("%6d%16.2f%18.6f%20.6f%18.6f\n", n, t, tsub, taj, tsob);
     }
+}
+
+void printsumasubmax2(){
+    double t =0; 
+    double tsub, taj, tsob;
 
     printf("Test con SumaSubMax2 \n");
-	printf("%4s%19s%21s%22s%18s \n", "n", "t(n)", "t(n)/n^0.75", "t(n)/n^0.95", "t(n)/n^1.15");
+	printf("%7s%17s%20s%20s%19s \n", "n", "t(n)", "t(n)/n^1.8", "t(n)/n^2", "t(n)/n^2.2");
 
-    n = 500;
-    while(n <= 32000){
+    for(int n = 500; n <= 32000; n*=2){
         int vector[n];
         t = testSumaSubmax2(vector, n);
 
         tsub=t/pow(n,0.75);
 		taj=t/pow(n,0.95);
 		tsob=t/pow(n,1.15);
-		printf("%5d%17.2f%18.6f%22.6f%18.6f\n", n, t, tsub, taj, tsob);
-
-
-        n*=2;
+		printf("%6d%16.2f%18.6f%20.6f%18.6f\n", n, t, tsub, taj, tsob);
     }
+}
 
+int main() {
+
+    //test con numeros dados
+    printTest1();
+    printf("\n");//separador
+
+    // test con numeros aleatorios
+    printAleatorio();
+    printf("\n");//separador
+
+    //tablas de tiempos
+    printsumasubmax1();
+    printf("\n");//separador
+    printsumasubmax2();
 
     return 0;
 }

543bb21581843ddde2659470f62ddcbd312973af
migueldeoleiros <migueldeoleiros@gmail.com>
2021-10-04 10:48:20 +0200

    practica 2

---
+++ p2/ordenacion.c
@@ -0,0 +1,6 @@
+// TITLE: Algoritmos Práctica 1
+// AUTHOR 1: Miguel López López        LOGIN 1: m.llopez
+// AUTHOR 2: Ángel Gómez prol          LOGIN 2: angel.gomez.prol
+// AUTHOR 3: Xoel Díaz Préstamo        LOGIN 3: xoel.diaz
+// GROUP: 2.1.4
+// DATE: 23/10/2021
